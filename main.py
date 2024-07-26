170c04fb015d33341e6e150cd9194bdd51a10cdb
ElliNet13 <elliotmoose4@gmail.com>
2024-07-26 10:36:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -23,16 +23,23 @@ def fetch_sitemap(sitemap_url):
         if response.status_code == 200:
             xml_text = response.text
             xml_data = ET.fromstring(xml_text)
-            urls = xml_data.findall('.//{http://www.sitemaps.org/schemas/sitemap/0.9}url')
 
             sitemap_data = []
+            # Process <url> elements
+            urls = xml_data.findall('.//{http://www.sitemaps.org/schemas/sitemap/0.9}url')
             for url in urls:
                 loc = url.find('{http://www.sitemaps.org/schemas/sitemap/0.9}loc').text
-                title = fetch_title(loc)
-                if title:
-                    sitemap_data.append({'title': title, 'link': loc})
-                else:
-                    sitemap_data.append({'title': loc, 'link': loc})
+                name = url.find('{http://www.sitemaps.org/schemas/sitemap/0.9}name')
+                title = name.text if name is not None and name.text else fetch_title(loc)
+                sitemap_data.append({'title': title if title else loc, 'link': loc})
+
+            # Process nested sitemaps
+            sitemaps = xml_data.findall('.//{http://www.sitemaps.org/schemas/sitemap/0.9}sitemap')
+            for sitemap in sitemaps:
+                loc = sitemap.find('{http://www.sitemaps.org/schemas/sitemap/0.9}loc').text
+                nested_sitemap_data = fetch_sitemap(loc)
+                if nested_sitemap_data:
+                    sitemap_data.extend(nested_sitemap_data)
 
             return sitemap_data
         else:
@@ -55,12 +62,9 @@ def fetch_title(url):
     try:
         response = requests.get(url)
         if response.status_code == 200:
-            html_text = response.text
-            soup = BeautifulSoup(html_text, 'html.parser')
-            title = soup.title.string
-            return title.strip() if title else None
+            soup = BeautifulSoup(response.text, 'html.parser')
+            return soup.title.string.strip() if soup.title else None
         else:
-            
             return None
     except Exception as e:
         print('Error fetching or parsing HTML page:', e)
@@ -77,12 +81,7 @@ def search_sitemap(sitemap_data, search_query):
     Returns:
         list: A list of dictionaries representing the matching sites.
     """
-    matching_sites = []
-    for site in sitemap_data:
-        if search_query.lower() in site['title'].lower():
-            matching_sites.append(site)
-
-    return matching_sites
+    return [site for site in sitemap_data if search_query.lower() in site['title'].lower()]
 
 def list_sites(sitemap_data):
     """
@@ -117,43 +116,28 @@ def select_site(sitemap_data):
         print("Invalid input. Please enter a number.")
         return None
 
-def get_site_link(sitemap_data, site_number):
-    """
-    Gets the link of the site corresponding to the specified site number.
-    
-    Args:
-        sitemap_data (list): A list of dictionaries containing the titles and links of the URLs in the sitemap.
-        site_number (int): The number of the site to get the link for.
-    
-    Returns:
-        str: The URL of the selected site, or None if the site number is invalid.
-    """
-    try:
-        site_index = int(site_number) - 1
-        if 0 <= site_index < len(sitemap_data):
-            return sitemap_data[site_index]['link']
+def main():
+    sitemap_url = input("Enter the URL of the sitemap XML (Leave empty to use https://ellinet13.github.io/sitemap.xml): ")
+    if not sitemap_url:
+        sitemap_url = "https://ellinet13.github.io/sitemap.xml"
+
+    print("Loading sitemap...")
+    sitemap_data = fetch_sitemap(sitemap_url)
+    if sitemap_data:
+        print("Done!")
+        search_query = input("Enter the search query (Leave empty to get all pages): ")
+        sitemap_data = search_sitemap(sitemap_data, search_query)
+        site_url = select_site(sitemap_data)
+        if site_url:
+            print("Page URL:", site_url)
+            open_link = input("Would you like to open it? (yes/no): ").strip().lower()
+            if open_link == 'yes':
+                import webbrowser
+                webbrowser.open(site_url)
         else:
-            print("Invalid site number.")
-            return None
-    except ValueError:
-        print("Invalid input. Please enter a number.")
-        return None
+            print("No site selected.")
+    else:
+        print("Failed to load sitemap.")
 
-# The code
-sitemap = input("Enter the URL of the sitemap XML (Leave empty to use https://ellinet13.github.io/sitemap.xml): ")
-if sitemap == "":
-    sitemap = "https://ellinet13.github.io/sitemap.xml"
-
-print("Loading sitemap...")
-sitemap_data = fetch_sitemap("https://ellinet13.github.io/sitemap.xml")
-print("Done!")
-sitemap_data = search_sitemap(sitemap_data, input("Enter the search query (Leave empty to get all pages): "))
-site_url = select_site(sitemap_data)
-if site_url:
-    print("Page URL:", site_url)
-    open_link = input("Would you like to open it? (yes/no): ").strip().lower()
-    if open_link == 'yes':
-        import webbrowser
-        webbrowser.open(site_url)
-else:
-    print("No site selected.")
+if __name__ == "__main__":
+    main()
